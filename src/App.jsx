ba70b92961af34d295d6ee54e70f465b3f85daa6
Thomas Hunter <thomashunter@Thomass-MacBook-Pro-3.local>
2024-03-25 14:11:15 -0700

    second push. I have the puppies on the page now

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import { useState } from 'react'
 import './App.css'
+import { puppyList } from './data.js'
+
+
 
 function App() {
-  const [] = useState(0)
-
+  const [puppies, setPuppies] = useState(puppyList)
   return (
     <>
-    
+    <div>
+      {
+        puppies.map((puppy) => {
+          return <p key={puppy.id}>{puppy.name}</p>
+        })
+      }
+
+    </div>
     </>
   )
 }
