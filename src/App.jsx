31e70e5924ce269ecd596a2a33119713c7a3aede
Thomas Hunter <thomashunter@Thomass-MacBook-Pro-3.local>
2024-03-25 19:40:10 -0700

    Wrote code to add event listeners to the names The event listener renders info for the puppy clicked on the page below the list

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,24 @@ import { puppyList } from './data.js'
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList)
+  const [featPupId, setFeatPupId] = useState(null);
+  const featuredPup = puppies.find((pup) => {
+    return pup.id === featPupId;})
+  console.log(featuredPup);
   return (
     <>
     <div>
       {
         puppies.map((puppy) => {
-          return <p key={puppy.id}>{puppy.name}</p>
+          return <p onClick = {() => {setFeatPupId(puppy.id)}}key={puppy.id}>{puppy.name}</p>
         })
       }
+      {featPupId && (
+        <div>
+          <h2>Name: {featuredPup.name}</h2>
+          <h3>Age: {featuredPup.age}</h3>
+          <h3>Contact: {featuredPup.email}</h3>
+        </div>)}
 
     </div>
     </>
